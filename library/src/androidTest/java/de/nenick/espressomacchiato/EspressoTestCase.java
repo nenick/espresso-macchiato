d0226011a46baae6fd8eef4975d1006bde09d8b0
nenick <kuchler.nico@gmail.com>
2016-03-10 23:40:21 +0100

    remove lock screen from emulator

--- library/src/androidTest/java/de/nenick/espressomacchiato/EspressoTestCase.java
+++ library/src/androidTest/java/de/nenick/espressomacchiato/EspressoTestCase.java
@@ -1,8 +1,12 @@
 package de.nenick.espressomacchiato;
 
+import android.app.Activity;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.WindowManager;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
@@ -14,4 +18,26 @@ public abstract class EspressoTestCase {
     @Rule
     public ActivityTestRule<BaseActivity> activityTestRule = new ActivityTestRule<>(BaseActivity.class);
 
+    @Before
+    public void setupEspresso() {
+        avoidLockScreen();
+    }
+
+    private void avoidLockScreen() {
+        // sometimes tests failed on emulator because lock screen is shown
+        // java.lang.RuntimeException: Waited for the root of the view hierarchy to have window focus and not be requesting layout for over 10 seconds. If you specified a non default root matcher, it may be picking a root that never takes focus. Otherwise, something is seriously wrong"
+        // http://stackoverflow.com/questions/26139070/testui-jenkins-using-espresso
+        // http://stackoverflow.com/questions/22737476/false-positives-junit-framework-assertionfailederror-edittext-is-not-found
+        // http://developer.android.com/reference/android/view/WindowManager.LayoutParams.html#FLAG_SHOW_WHEN_LOCKED
+        InstrumentationRegistry.getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                Activity activity = activityTestRule.getActivity();
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        });
+    }
 }
