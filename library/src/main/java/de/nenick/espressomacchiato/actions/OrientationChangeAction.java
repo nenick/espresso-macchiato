13941e8279d6142475b4dd9fd04a70bc7667ca91
nenick <kuchler.nico@gmail.com>
2016-03-13 10:41:50 +0100

    remove unused imports

--- library/src/main/java/de/nenick/espressomacchiato/actions/OrientationChangeAction.java
+++ library/src/main/java/de/nenick/espressomacchiato/actions/OrientationChangeAction.java
@@ -7,14 +7,10 @@ import android.content.res.Configuration;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
-import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
-import android.support.test.runner.lifecycle.Stage;
 import android.util.Log;
 import android.view.View;
 
 import org.hamcrest.Matcher;
-
-import java.util.Collection;
 
 import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 
