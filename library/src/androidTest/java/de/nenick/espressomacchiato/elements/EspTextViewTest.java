c2de451f7efb65aa3aa9dd6d68af1dde2b71a961
nenick <kuchler.nico@gmail.com>
2016-03-13 10:09:06 +0100

    add test for esp text view

---
+++ library/src/androidTest/java/de/nenick/espressomacchiato/elements/EspTextViewTest.java
@@ -0,0 +1,29 @@
+package de.nenick.espressomacchiato.elements;
+
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.nenick.espressomacchiato.test.views.BaseActivity;
+import de.nenick.espressotools.EspressoTestCase;
+
+public class EspTextViewTest extends EspressoTestCase<BaseActivity> {
+
+    private String textViewText = "my text";
+    private int textViewId = android.R.id.text1;
+    private EspTextView espTextView = new EspTextView(textViewId);
+
+    @Before
+    public void setup() {
+        TextView textView = new TextView(activityTestRule.getActivity());
+        textView.setId(textViewId);
+        textView.setText(textViewText);
+        addViewToActivity(textView, BaseActivity.linearLayout);
+    }
+
+    @Test
+    public void testReplaceText() {
+        espTextView.assertTextIs(textViewText);
+    }
+}
