ca56c7a747443bbea698a3d1e7774f2868990c15
nenick <kuchler.nico@gmail.com>
2016-03-18 19:19:28 +0100

    reset orientation state after test

--- library/src/androidTest/java/de/nenick/espressomacchiato/elements/EspDeviceOrientationFixedTest.java
+++ library/src/androidTest/java/de/nenick/espressomacchiato/elements/EspDeviceOrientationFixedTest.java
@@ -1,13 +1,24 @@
 package de.nenick.espressomacchiato.elements;
 
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.After;
 import org.junit.Test;
 
+import de.nenick.espressomacchiato.test.views.BaseActivity;
 import de.nenick.espressomacchiato.test.views.LandscapeFixedActivity;
 import de.nenick.espressotools.EspressoTestCase;
 
 public class EspDeviceOrientationFixedTest extends EspressoTestCase<LandscapeFixedActivity> {
 
     private EspDevice espDevice = EspDevice.root();
+
+    @After
+    public void reset() {
+        activityTestRule.getActivity().startActivity(new Intent(InstrumentationRegistry.getTargetContext(), BaseActivity.class));
+        espDevice.rotateToPortrait();
+    }
 
     @Test
     public void testRotateOnOrientationFixedActivity() {
