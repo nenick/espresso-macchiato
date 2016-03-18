65bcdebe6a06384f1dc560ac1c273f400b54383e
nenick <kuchler.nico@gmail.com>
2016-03-18 08:22:59 +0100

    try to stabilize keyboard assertions on emulator

--- library/src/main/java/de/nenick/espressomacchiato/elements/EspDevice.java
+++ library/src/main/java/de/nenick/espressomacchiato/elements/EspDevice.java
@@ -89,23 +89,38 @@ public class EspDevice {
         assertSoftKeyboardIsOpen(false);
     }
 
-    protected void assertSoftKeyboardIsOpen(final boolean isShown) {
+    protected void assertSoftKeyboardIsOpen(final boolean expectIsShown) {
+        // give keyboard some initial time for hide/show actions on emulator
+        // didn't found another way to sync with keyboard actions
+        try {
+            // wait below 300ms was to less, keyboard actions may consume more time on emulator
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
         // original solution http://stackoverflow.com/a/26964010/3619179
         onView(isRoot()).check(new ViewAssertion() {
             @Override
             public void check(View view, NoMatchingViewException noViewFoundException) {
+                boolean isShown = calculateIsKeyboardShown(view);
+                if (isShown) {
+                    assertThat("Keyboard should be closed.", expectIsShown, is(true));
+                }
+                else {
+                    assertThat("Keyboard should be open.", !expectIsShown, is(true));
+                }
+            }
+
+            protected boolean calculateIsKeyboardShown(View view) {
                 Rect visibleFrameSpace = new Rect();
                 view.getWindowVisibleDisplayFrame(visibleFrameSpace);
                 int screenHeight = view.getHeight();
                 int keypadHeight = screenHeight - visibleFrameSpace.bottom;
 
-                // 0.15 ratio is perhaps enough to determine keypad height.
-                if (keypadHeight > screenHeight * 0.15) {
-                    assertThat("Keyboard should be closed.", isShown, is(true));
-                }
-                else {
-                    assertThat("Keyboard should be open.", !isShown, is(true));
-                }
+                // 0.15 ratio is perhaps enough to determine keypad height
+                double keypadMinHeight = screenHeight * 0.15;
+                return keypadHeight > keypadMinHeight;
             }
         });
     }
