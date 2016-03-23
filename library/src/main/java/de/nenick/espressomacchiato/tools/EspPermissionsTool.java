6ee785b878f788831a6c204d31ff4acf3846d03f
nenick <kuchler.nico@gmail.com>
2016-03-23 07:15:52 +0100

    try more sleep to stabilize test

--- library/src/main/java/de/nenick/espressomacchiato/tools/EspPermissionsTool.java
+++ library/src/main/java/de/nenick/espressomacchiato/tools/EspPermissionsTool.java
@@ -13,7 +13,7 @@ public class EspPermissionsTool {
     /**
      * wait below 2000ms was sometimes not enough for reset all permissions on circle ci emulator
      */
-    public static int DELAY_FOR_COMMAND_EXECUTION = 2000;
+    public static int DELAY_FOR_COMMAND_EXECUTION = 5000;
 
     /**
      * Safe way to remove granted permission to you app without app restart.
