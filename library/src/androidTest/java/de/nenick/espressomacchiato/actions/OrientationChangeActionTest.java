76391d9c1e7271eccc8cb00c28379c0069e6a7bc
nenick <kuchler.nico@gmail.com>
2016-04-13 16:02:19 +0200

    try if test has side effects for following tests

--- library/src/androidTest/java/de/nenick/espressomacchiato/actions/OrientationChangeActionTest.java
+++ library/src/androidTest/java/de/nenick/espressomacchiato/actions/OrientationChangeActionTest.java
@@ -3,11 +3,13 @@ package de.nenick.espressomacchiato.actions;
 import android.app.Activity;
 import android.support.test.InstrumentationRegistry;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import de.nenick.espressomacchiato.test.views.BaseActivity;
 import de.nenick.espressomacchiato.testbase.EspressoTestCase;
 
+@Ignore
 public class OrientationChangeActionTest extends EspressoTestCase<BaseActivity> {
 
     private OrientationChangeAction orientationChangeAction = OrientationChangeAction.orientationLandscape();
