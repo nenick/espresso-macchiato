68019a83d4177d8afd69378240be3c274bce0e61
Nico Küchler <nico.kuechler@epost-dev.de>
2016-05-12 20:16:30 +0200

    stabilize image comparison

--- espresso-macchiato/src/main/java/de/nenick/espressomacchiato/tools/EspScreenshotTool.java
+++ espresso-macchiato/src/main/java/de/nenick/espressomacchiato/tools/EspScreenshotTool.java
@@ -25,7 +25,7 @@ public class EspScreenshotTool {
      *
      * Actionbar contains a time which must be ignored.
      */
-    public static final double COMPARE_DELTA_TIME_CHANGE = 0.02;
+    public static final double COMPARE_DELTA_TIME_CHANGE = 0.03;
 
     /**
      * This folder will be created in your application data to store screenshots.
