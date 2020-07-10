6afa18591818e382e3f5cf50f582d1d5b352d5ea
Nico Küchler <kuchler.nico@gmail.com>
2020-07-10 22:47:42 +0200

    fix emulator startup

--- pipelines/templates/android-emulator.sh
+++ pipelines/templates/android-emulator.sh
@@ -6,22 +6,18 @@
 # Configuration
 EMULATOR_CONFIG="system-images;$1;default;x86"
 
-echo "Installing emulator"
-
 # Install AVD files
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install ${EMULATOR_CONFIG}
 
+# Create emulator
+echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n xamarin_android_emulator -k ${EMULATOR_CONFIG} --force
+
+$ANDROID_HOME/emulator/emulator -list-avds
+
 echo "Starting emulator"
 
-# Create emulator
-echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n android_emulator -k ${EMULATOR_CONFIG} --force
-
 # Start emulator in background
-nohup $ANDROID_HOME/emulator/emulator -avd android_emulator -no-snapshot > /dev/null 2>&1 &
-
-echo "Waiting until emulator is up"
-
-# Wait until emulator is started
+nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot > /dev/null 2>&1 &
 $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 
 $ANDROID_HOME/platform-tools/adb devices
