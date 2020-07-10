3ff4a54b65a1ecde3763b3671831601070f85492
Nico Küchler <kuchler.nico@gmail.com>
2020-07-10 21:59:31 +0200

    tru default start script

--- pipelines/templates/android-emulator.sh
+++ pipelines/templates/android-emulator.sh
@@ -1,30 +1,19 @@
 #!/usr/bin/env bash
 
-# Validate arguments
-[ -z "$1" ] && echo "Missing argument for android version (e.g. android-27)" && exit 1
-
-# Configuration
-EMULATOR_CONFIG="system-images;$1;default;x86"
-
-echo "Installing emulator"
-
 # Install AVD files
-echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install ${EMULATOR_CONFIG}
+echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-27;google_apis;x86'
 
 # Create emulator
-echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n android_emulator -k ${EMULATOR_CONFIG} --force
+echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n xamarin_android_emulator -k 'system-images;android-27;google_apis;x86' --force
+
+$ANDROID_HOME/emulator/emulator -list-avds
 
 echo "Starting emulator"
 
 # Start emulator in background
-nohup $ANDROID_HOME/emulator/emulator -avd android_emulator -no-snapshot > /dev/null 2>&1 &
-
-# Wait until emulator is started
+nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot > /dev/null 2>&1 &
 $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 
 $ANDROID_HOME/platform-tools/adb devices
 
 echo "Emulator started"
-
-# Give Emulator a few seconds to be really ready
-sleep 10
