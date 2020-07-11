45c45c8590d9289adfcf4bb1b36b63188e6dbe89
Nico Küchler <kuchler.nico@gmail.com>
2020-07-11 05:24:13 +0200

    sed instead of tr

--- pipelines/templates/android-emulator.sh
+++ pipelines/templates/android-emulator.sh
@@ -6,8 +6,6 @@
 # Configuration
 EMULATOR_CONFIG="system-images;$1;default;x86"
 
-echo "Starting emulator $1"
-
 # Install AVD files
 echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install "${EMULATOR_CONFIG}"
 
@@ -20,7 +18,7 @@ echo "Starting emulator"
 
 # Start emulator in background
 nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot > /dev/null 2>&1 &
-$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | sed 's/\r//g') ]]; do sleep 1; done; input keyevent 82'
 
 $ANDROID_HOME/platform-tools/adb devices
 
