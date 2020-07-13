72b9b8ca71e6e2010793df28a9ada7750e0db594
Nico Küchler <kuchler.nico@gmail.com>
2020-07-13 19:37:19 +0200

    collect emulator logs

--- pipelines/templates/android-emulator.sh
+++ pipelines/templates/android-emulator.sh
@@ -39,3 +39,7 @@ sleep 10
 $ANDROID_HOME/platform-tools/adb shell settings put global window_animation_scale 0
 $ANDROID_HOME/platform-tools/adb shell settings put global transition_animation_scale 0
 $ANDROID_HOME/platform-tools/adb shell settings put global animator_duration_scale 0
+
+################################################################################
+# Clear logs from emulator, because we want to download only test related logs.
+$ANDROID_HOME/platform-tools/adb logcat -c
