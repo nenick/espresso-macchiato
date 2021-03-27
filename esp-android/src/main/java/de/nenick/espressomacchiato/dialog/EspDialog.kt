9663a7ece1ef9a32025f44bfc587e5af0dc53111
Nico Küchler <kuchler.nico@gmail.com>
2021-03-27 11:33:25 +0100

    Reduce method scope when using dialog page object pattern

--- esp-android/src/main/java/de/nenick/espressomacchiato/dialog/EspDialog.kt
+++ esp-android/src/main/java/de/nenick/espressomacchiato/dialog/EspDialog.kt
@@ -2,7 +2,11 @@ package de.nenick.espressomacchiato.dialog
 
 import de.nenick.espressomacchiato.dialoginteraction.DialogVisibilityAssertions
 import de.nenick.espressomacchiato.internals.OpenForExtensions
+import de.nenick.espressomacchiato.internals.ScopeDslMarker
 
+// Scope marker reduces the amount methods when views get nested e.g. the page style.
+// Result is in sub lambda blocks you don't have access to the parent lambda block methods.
+@ScopeDslMarker
 @Suppress("FunctionName")
 @OpenForExtensions
 class EspDialog(block: EspDialog.() -> Unit = {}) :
