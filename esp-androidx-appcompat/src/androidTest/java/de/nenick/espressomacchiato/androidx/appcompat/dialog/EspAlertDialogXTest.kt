d94fcd371baaae186a249895eb62573533b0004a
Nico Küchler <kuchler.nico@gmail.com>
2021-03-27 21:01:05 +0100

    Add androidx alert dialog element

---
+++ esp-androidx-appcompat/src/androidTest/java/de/nenick/espressomacchiato/androidx/appcompat/dialog/EspAlertDialogXTest.kt
@@ -0,0 +1,59 @@
+package de.nenick.espressomacchiato.androidx.appcompat.dialog
+
+import androidx.appcompat.app.AlertDialog
+import de.nenick.espressomacchiato.dialog.EspAlertDialog
+import de.nenick.espressomacchiato.test.core.BaseActivityTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class EspAlertDialogXTest : BaseActivityTest() {
+
+    private var clicked = ""
+    private var dismissed = false
+
+    @Test
+    fun example() {
+        showDialog()
+        EspAlertDialogX {
+            checkIsDisplayed()
+
+            Title { checkText("My title") }
+            Message { checkText("My message") }
+
+            PositiveButton {
+                checkText("Positive")
+                performClick()
+            }
+
+            checkNotDisplayed()
+        }
+        assertEquals("Positive", clicked)
+
+        showDialog()
+        EspAlertDialog().NegativeButton {
+            checkText("Negative")
+            performClick()
+        }
+        assertEquals("Negative", clicked)
+
+        showDialog()
+        EspAlertDialog().NeutralButton {
+            checkText("Neutral")
+            performClick()
+        }
+        assertEquals("Neutral", clicked)
+    }
+
+    private fun showDialog() {
+        runOnMainSync {
+            AlertDialog.Builder(it)
+                    .setTitle("My title")
+                    .setMessage("My message")
+                    .setNegativeButton("Negative") { _, _ -> clicked = "Negative" }
+                    .setNeutralButton("Neutral") { _, _ -> clicked = "Neutral" }
+                    .setPositiveButton("Positive") { _, _ -> clicked = "Positive" }
+                    .setOnDismissListener { dismissed = true }
+                    .show()
+        }
+    }
+}
