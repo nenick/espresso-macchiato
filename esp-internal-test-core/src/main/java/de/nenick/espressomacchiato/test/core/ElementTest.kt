e88886eefeeb4b33ba6a0cfbee71ce324cc9e97f
Nico Küchler <kuchler.nico@gmail.com>
2021-03-27 23:12:36 +0100

    Fix reflection based tests on earlier android versions

--- esp-internal-test-core/src/main/java/de/nenick/espressomacchiato/test/core/ElementTest.kt
+++ esp-internal-test-core/src/main/java/de/nenick/espressomacchiato/test/core/ElementTest.kt
@@ -1,15 +1,15 @@
 package de.nenick.espressomacchiato.test.core
 
-import android.annotation.SuppressLint
 import android.app.Activity
+import android.os.Build
 import android.view.View
 import android.view.ViewGroup
 import androidx.test.espresso.Root
 import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import de.nenick.espressomacchiato.view.EspView
 import de.nenick.espressomacchiato.internals.OpenForExtensions
+import de.nenick.espressomacchiato.view.EspView
 import org.hamcrest.Matcher
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
@@ -17,16 +17,13 @@ import org.junit.Before
 import org.junit.Test
 import java.lang.reflect.ParameterizedType
 
-// It's complaining here but not when the "NewApi" usage is inside androidTest folder.
-// This code is only targeting testing so we ignore this warning. Interestingly it works.
-@SuppressLint("NewApi")
 abstract class ElementTest<ELEMENT : EspView, TEST_ACTIVITY : Activity> : BaseActivityTest<TEST_ACTIVITY>() {
 
     val testViewId: Int = android.R.id.home
     val testView: View = View(context).apply {
         id = testViewId
         layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 50)
-        setBackgroundColor(context.getColor(android.R.color.holo_green_dark))
+        setBackgroundColor(context.resources.getColor(android.R.color.holo_green_dark))
     }
 
     private val elementTypeUnderTest = thisGenericParameterType()
@@ -54,9 +51,9 @@ abstract class ElementTest<ELEMENT : EspView, TEST_ACTIVITY : Activity> : BaseAc
     fun hasConstructorForAllSituations() {
         val constructor = elementTypeUnderTest.getConstructor(Matcher::class.java, Matcher::class.java, Function1::class.java)
 
-        assertEquals(parameterizedType<Matcher<View>>(), constructor.parameters[0].parameterizedType)
-        assertEquals(parameterizedType<Matcher<Root>>(), constructor.parameters[1].parameterizedType)
-        assertEquals(parameterizedType<Function1<ELEMENT, Unit>>(), constructor.parameters[2].parameterizedType)
+        assertEquals(parameterizedType<Matcher<View>>(), constructor.genericParameterTypes[0])
+        assertEquals(parameterizedType<Matcher<Root>>(), constructor.genericParameterTypes[1])
+        assertEquals(parameterizedType<Function1<ELEMENT, Unit>>(), constructor.genericParameterTypes[2])
 
         val elementInstance = constructor.newInstance(withId(testViewId), null, { _: ELEMENT -> })
         elementInstance.check(isDisplayed())
@@ -65,8 +62,8 @@ abstract class ElementTest<ELEMENT : EspView, TEST_ACTIVITY : Activity> : BaseAc
     fun hasConstructorForViewId() {
         val constructor = elementTypeUnderTest.getConstructor(Int::class.java, Function1::class.java)
 
-        assertEquals(Int::class.java, constructor.parameters[0].type)
-        assertEquals(parameterizedType<Function1<ELEMENT, Unit>>(), constructor.parameters[1].parameterizedType)
+        assertEquals(Int::class.java, constructor.parameterTypes[0])
+        assertEquals(parameterizedType<Function1<ELEMENT, Unit>>(), constructor.genericParameterTypes[1])
 
         val elementInstance = constructor.newInstance(testViewId, { _: ELEMENT -> })
         elementInstance.check(isDisplayed())
@@ -75,9 +72,9 @@ abstract class ElementTest<ELEMENT : EspView, TEST_ACTIVITY : Activity> : BaseAc
     fun hasConstructorForViewIdWithCustomRoot() {
         val constructor = elementTypeUnderTest.getConstructor(Int::class.java, Matcher::class.java, Function1::class.java)
 
-        assertEquals(Int::class.java, constructor.parameters[0].type)
-        assertEquals(parameterizedType<Matcher<Root>>(), constructor.parameters[1].parameterizedType)
-        assertEquals(parameterizedType<Function1<ELEMENT, Unit>>(), constructor.parameters[2].parameterizedType)
+        assertEquals(Int::class.java, constructor.parameterTypes[0])
+        assertEquals(parameterizedType<Matcher<Root>>(), constructor.genericParameterTypes[1])
+        assertEquals(parameterizedType<Function1<ELEMENT, Unit>>(), constructor.genericParameterTypes[2])
 
         val elementInstance = constructor.newInstance(testViewId, RootMatchers.DEFAULT, { _: ELEMENT -> })
         elementInstance.check(isDisplayed())
@@ -86,8 +83,8 @@ abstract class ElementTest<ELEMENT : EspView, TEST_ACTIVITY : Activity> : BaseAc
     fun hasConstructorForCustomViewMatcher() {
         val constructor = elementTypeUnderTest.getConstructor(Matcher::class.java, Function1::class.java)
 
-        assertEquals(parameterizedType<Matcher<View>>(), constructor.parameters[0].parameterizedType)
-        assertEquals(parameterizedType<Function1<ELEMENT, Unit>>(), constructor.parameters[1].parameterizedType)
+        assertEquals(parameterizedType<Matcher<View>>(), constructor.genericParameterTypes[0])
+        assertEquals(parameterizedType<Function1<ELEMENT, Unit>>(), constructor.genericParameterTypes[1])
 
         val elementInstance = constructor.newInstance(withId(testViewId), { _: ELEMENT -> })
         elementInstance.check(isDisplayed())
@@ -100,7 +97,7 @@ abstract class ElementTest<ELEMENT : EspView, TEST_ACTIVITY : Activity> : BaseAc
         return thisGenericClassType.actualTypeArguments[0] as Class<ELEMENT>
     }
 
-    private inline fun <reified T> parameterizedType() = object : GenericTypeResolver<T>("ELEMENT", elementTypeUnderTest) {}
+    private inline fun <reified T> parameterizedType() = object : GenericTypeResolver<T>("ELEMENT", elementTypeUnderTest.name) {}
 }
 
 
--- esp-internal-test-core/src/main/java/de/nenick/espressomacchiato/test/core/GenericTypeResolver.kt
+++ esp-internal-test-core/src/main/java/de/nenick/espressomacchiato/test/core/GenericTypeResolver.kt
@@ -6,13 +6,13 @@ import java.lang.reflect.Type
 /** Helper to create [Type] references for generic classes. */
 abstract class GenericTypeResolver<out A>(
         private val lambdaGenericArgumentName: String? = null,
-        private val lambdaGenericArgumentType: Type? = null
+        private val lambdaGenericArgumentType: String? = null
 ) : Type {
 
     override fun getTypeName(): String {
         val thisGenericTypeResolver = this::class.java.genericSuperclass as ParameterizedType
         val genericToResolve = thisGenericTypeResolver.actualTypeArguments[0] as ParameterizedType
-        return genericToResolve.typeName
+        return genericToResolve.toString()
                 .optionalFixFunctionUnitType()
                 .optionalFixFunctionGenericArgumentType()
     }
@@ -23,7 +23,7 @@ abstract class GenericTypeResolver<out A>(
 
     override fun equals(other: Any?): Boolean {
         if (other !is ParameterizedType) return false
-        return typeName == other.typeName
+        return typeName == other.toString()
     }
 
     override fun hashCode(): Int {
@@ -42,9 +42,12 @@ abstract class GenericTypeResolver<out A>(
      * something with "? extends GENERIC_TYP_NAME" but Constructor<T>.parameters[..].parameterizedType
      * does correctly produce "? extends ExplicitClassType". So we adjust our result to match the more real behavior.
      */
-    private fun String.optionalFixFunctionGenericArgumentType() = replace("? super $lambdaGenericArgumentName", "? super ${lambdaGenericArgumentType?.typeName}")
+    private fun String.optionalFixFunctionGenericArgumentType() = replace("? super $lambdaGenericArgumentName", "? super ${lambdaGenericArgumentType}")
 
     fun isGeneric() {
+        // java.lang.AssertionError:
+        // expected:<kotlin.jvm.functions.Function1<? super class de.nenick.espressomacchiato.view.EspView, kotlin.Unit>>
+        // but was: <kotlin.jvm.functions.Function1<? super de.nenick.espressomacchiato.view.EspView, kotlin.Unit>>
 
     }
 }
