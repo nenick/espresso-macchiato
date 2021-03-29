5d05ebfb716d2620f19a3d51dc51a35eae11e456
Nico Küchler <kuchler.nico@gmail.com>
2021-03-29 13:51:24 +0200

    Use helper to perform interactions

--- esp-android/src/main/java/de/nenick/espressomacchiato/viewinteraction/VisibilityAssertions.kt
+++ esp-android/src/main/java/de/nenick/espressomacchiato/viewinteraction/VisibilityAssertions.kt
@@ -1,33 +1,27 @@
 package de.nenick.espressomacchiato.viewinteraction
 
-import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.Visibility.GONE
-import androidx.test.espresso.matcher.ViewMatchers.Visibility.INVISIBLE
-import androidx.test.espresso.matcher.ViewMatchers.Visibility.VISIBLE
-import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast
-import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.Visibility.*
 import de.nenick.espressomacchiato.EspGlobalSettings
+import de.nenick.espressomacchiato.internals.HasChecks
+import de.nenick.espressomacchiato.internals.HasInteractions
 import org.hamcrest.CoreMatchers.anyOf
 import org.hamcrest.CoreMatchers.not
 
-interface VisibilityAssertions {
+interface VisibilityAssertions : HasChecks, HasInteractions {
 
-    fun onView(): ViewInteraction
+    fun checkDoesNotExist() = onView().check(doesNotExist())!!
 
-    fun checkDoesNotExist() = onView().check(doesNotExist())
+    fun checkIsDisplayed() = check(EspGlobalSettings.isDisplayedCheckStrategy)
+    fun checkIsCompletelyDisplayed() = check(isCompletelyDisplayed())
 
-    fun checkIsDisplayed() = onView().check(matches(EspGlobalSettings.isDisplayedCheckStrategy))
-    fun checkIsCompletlyDisplayed() = onView().check(matches(isCompletelyDisplayed()))
+    fun checkIsVisible() = check(withEffectiveVisibility(VISIBLE))
+    fun checkIsInvisible() = check(withEffectiveVisibility(INVISIBLE))
+    fun checkIsGone() = check(withEffectiveVisibility(GONE))
 
-    fun checkIsVisible() = onView().check(matches(withEffectiveVisibility(VISIBLE)))
-    fun checkIsInvisible() = onView().check(matches(withEffectiveVisibility(INVISIBLE)))
-    fun checkIsGone() = onView().check(matches(withEffectiveVisibility(GONE)))
-
-    fun checkIsHidden() = onView().check(matches(anyOf(
+    fun checkIsHidden() = check(anyOf(
             withEffectiveVisibility(INVISIBLE),
             withEffectiveVisibility(GONE),
-            not(isDisplayingAtLeast(1)))))
+            not(isDisplayingAtLeast(1))))
 }
