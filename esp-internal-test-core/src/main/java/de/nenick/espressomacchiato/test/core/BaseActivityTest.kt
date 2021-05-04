8ebd4c0c8dcaca4ce4c7d4798e324fcf65d044f7
Nico Küchler <kuchler.nico@gmail.com>
2021-05-04 13:00:06 +0200

    Apply SecurityException workaround for test runs

--- esp-internal-test-core/src/main/java/de/nenick/espressomacchiato/test/core/BaseActivityTest.kt
+++ esp-internal-test-core/src/main/java/de/nenick/espressomacchiato/test/core/BaseActivityTest.kt
@@ -1,30 +1,38 @@
 package de.nenick.espressomacchiato.test.core
 
 import android.app.Activity
+import android.os.Build
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.IdRes
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import de.nenick.espressomacchiato.screenshot.EspScreenshotRule
+import org.junit.After
 import org.junit.Rule
 import org.junit.rules.RuleChain
 
 abstract class BaseActivityTest<TEST_ACTIVITY : Activity> {
 
+    abstract val activityScenarioRule: ActivityScenarioRule<TEST_ACTIVITY>
     private val screenshotRule = EspScreenshotRule()
 
     @get:Rule
     val chain by lazy {
         RuleChain.emptyRuleChain()
-                .around(activityScenarioRule)
-                .around(screenshotRule)!!
+            .around(activityScenarioRule)
+            .around(screenshotRule)!!
     }
 
-    abstract val activityScenarioRule: ActivityScenarioRule<TEST_ACTIVITY>
-
-    private val instrumentation = InstrumentationRegistry.getInstrumentation()
     val context = InstrumentationRegistry.getInstrumentation().targetContext!!
+
+    @After
+    fun baseTearDown() {
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            // Would otherwise randomly crash the instrumentation test run.
+            SecurityExceptionFix.apply()
+        }
+    }
 
     fun addViewToRoot(view: View) {
         addViewToLayout(view, BaseActivity.rootLayout)
---
+++ esp-internal-test-core/src/main/java/de/nenick/espressomacchiato/test/core/SecurityExceptionFix.kt
@@ -0,0 +1,50 @@
+package de.nenick.espressomacchiato.test.core
+
+import android.util.Log
+
+object SecurityExceptionFix {
+
+    private lateinit var originExceptionHandler: Thread.UncaughtExceptionHandler
+    private val ignoringSecurityExceptionHandler = Thread.UncaughtExceptionHandler { t, e ->
+        if (isNotTrustedUidException(e)) {
+            // Dirty hack to avoid random Instrumentation run crashes.
+            //
+            //     4.3.1 failed: Instrumentation run failed due to 'java.lang.SecurityException'
+            //     4.3.1 failed: Instrumentation run failed due to 'Process crashed.'
+            //
+            // java.lang.SecurityException: Calling from not trusted UID!
+            //     at android.os.Parcel.readException(Parcel.java:1431)
+            //     at android.os.Parcel.readException(Parcel.java:1385)
+            //     at android.app.ActivityManagerProxy.finishInstrumentation(ActivityManagerNative.java:2954)
+            //     at android.app.ActivityThread.finishInstrumentation(ActivityThread.java:4466)
+            //     at android.app.Instrumentation.finish(Instrumentation.java:196)
+            //     at androidx.test.runner.MonitoringInstrumentation.finish(MonitoringInstrumentation.java:367)
+            //     at androidx.test.runner.AndroidJUnitRunner.finish(AndroidJUnitRunner.java:415)
+            //     at androidx.test.runner.AndroidJUnitRunner.onStart(AndroidJUnitRunner.java:404)
+            //     at android.app.Instrumentation$InstrumentationThread.run(Instrumentation.java:1701)
+            //
+            // Happens at the last cleanup action. Just global ignoring it does result into additional crash log.
+            //
+            //     system_process W/ActivityManager: Error in app ...
+            //     system_process W/ActivityManager: Crash of app ...
+            //
+            // Didn't found any bad side effects by just ignoring it, yet. Other option would be to write a custom
+            // AndroidJUnitRunner and catch the SecurityException by overriding the finish(Int, Bundle?) method.
+            Log.w("AndroidJUnitRunner", "Ignored random SecurityException when instrumentation finishes.", e)
+        } else {
+            originExceptionHandler.uncaughtException(t, e)
+        }
+    }
+
+    private fun isNotTrustedUidException(e: Throwable) = e is SecurityException &&
+        e.message == "Calling from not trusted UID!" &&
+        e.stackTraceToString().contains("androidx.test.runner.AndroidJUnitRunner.finish")
+
+    fun apply() {
+        val currentExceptionHandler = Thread.getDefaultUncaughtExceptionHandler()
+        if (currentExceptionHandler != ignoringSecurityExceptionHandler) {
+            originExceptionHandler = currentExceptionHandler!!
+            Thread.setDefaultUncaughtExceptionHandler(ignoringSecurityExceptionHandler)
+        }
+    }
+}
